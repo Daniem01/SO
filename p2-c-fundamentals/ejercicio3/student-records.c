66ff2df1228694e0b056a88a93b8301f98a4361d
Daniem01 <danimartindc01@gmail.com>
2025-12-14 17:57:03 +0100

    2.3 completo

--- p2-c-fundamentals/ejercicio3/student-records.c
+++ p2-c-fundamentals/ejercicio3/student-records.c
@@ -4,19 +4,35 @@
 #include <string.h>
 #include "defs.h"
 
+extern char *optarg;
 
 /* Assume lines in the text file are no larger than 100 chars */
 #define MAXLEN_LINE_FILE 100
 
 int print_text_file(char *path)
 {
-	SimpleRecord a[3];
+	SimpleRecord t;
+	FILE *fd;
+	int s, c;
 
-	// Inicializamos el array con valores de ejemplo
-	for(int i = 0; i < 3; i++){
-		a[i].id = i;
-		a[i].value = 0;
-		a[i].label[0] = '\0';
+	// Abrimos el archivo con el path que nos pasan
+	if ((fd = fopen(path, "r")) == NULL)
+	{
+		perror("Error abriendo el archivo con fopen.");
+		return -1;
+	}
+
+	// Hacemos el fscanf y los mostramos por pantalla
+	while ((s = fscanf(fd, "%d %lf %s", &t.id, &t.value, t.label)) == 3)
+	{
+		printf("Id: %d, Valor: %lf, Etiqueta: %s \n", t.id, t.value, t.label);
+	}
+
+	// Cerramos
+	if ((c = fclose(fd)) == EOF)
+	{
+		perror("Error cerrando el fichero.");
+		return -1;
 	}
 
 	return 0;
@@ -24,14 +40,73 @@ int print_text_file(char *path)
 
 int print_binary_file(char *path)
 {
-	/* To be completed  (part B) */
+	FILE *fd;
+	int r, c;
+	SimpleRecord sr;
+
+	// Abrimos el archivo con el path que nos pasan
+	if ((fd = fopen(path, "rb")) == NULL)
+	{
+		perror("Error abriendo el archivo con fopen.");
+		return -1;
+	}
+
+	// Leemos lo que contiene el archivo
+	while ((r = fread(&sr, sizeof(SimpleRecord), 1, fd)) == 1)
+	{
+		printf("Id: %d, Valor: %lf, Etiqueta: %s \n", sr.id, sr.value, sr.label);
+	}
+
+	// Cerramos
+	if ((c = fclose(fd)) == EOF)
+	{
+		perror("Error cerrando el fichero.");
+		return -1;
+	}
+
 	return 0;
 }
 
-
 int write_binary_file(char *input_file, char *output_file)
 {
-	/* To be completed  (part C) */
+	FILE *fo, *fi;
+	SimpleRecord t;
+	int s, w, c1, c2;
+
+	// Abrimos el archivo con el path que nos pasan
+	if ((fi = fopen(input_file, "r")) == NULL)
+	{
+		perror("Error abriendo el archivo input.");
+		return -1;
+	}
+	if ((fo = fopen(output_file, "wb")) == NULL)
+	{
+		perror("Error abriendo el archivo output.");
+		return -1;
+	}
+
+	// Usamos fsacanf para la entrada y vamos escribiendo en la salida
+	while ((s = fscanf(fi, "%d %lf %s", &t.id, &t.value, t.label)) == 3)
+	{
+		if ((w = fwrite(&t, sizeof(SimpleRecord), 1, fo)) != 1)
+		{
+			perror("Error escribiendo en el output_file.");
+			return -1;
+		}
+	}
+
+	// Cerramos los archivos
+	if ((c1 = fclose(fi)) == EOF)
+	{
+		perror("Error cerrando el fichero.");
+		return -1;
+	}
+	if ((c2 = fclose(fo)) == EOF)
+	{
+		perror("Error cerrando el fichero.");
+		return -1;
+	}
+
 	return 0;
 }
 
@@ -47,7 +122,7 @@ int main(int argc, char *argv[])
 	ret_code = 0;
 
 	/* Parse command-line options (incomplete code!) */
-	while ((opt = getopt(argc, argv, "hi:")) != -1)
+	while ((opt = getopt(argc, argv, "hi:pbo:")) != -1)
 	{
 		switch (opt)
 		{
@@ -57,11 +132,16 @@ int main(int argc, char *argv[])
 		case 'i':
 			options.input_file = optarg;
 			break;
-		
-		/**
-		 **  To be completed ...
-		 **/
-
+		case 'p':
+			options.action = PRINT_TEXT_ACT;
+			break;
+		case 'b':
+			options.action = PRINT_BINARY_ACT;
+			break;
+		case 'o':
+			options.output_file = optarg;
+			options.action = WRITE_BINARY_ACT;
+			break;
 		default:
 			exit(EXIT_FAILURE);
 		}
